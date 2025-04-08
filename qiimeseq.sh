f38eff8c79ae9e9b7c1319c6bd316e38bce33654
luisxxu <114445896+luisxxu@users.noreply.github.com>
2025-04-08 01:21:11 -0700

    Add files via upload

---
+++ qiimeseq.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+toolkitpath=$1
+
+export PATH=${HOME}/edirect:${PATH}
+export PATH=$toolkitpath:$PATH
+
+# prompt user for info
+read -p "Study name (e.g. ColstonJackson-et-al-2021) (NO UNDERSCORES): " name
+read -p "Country: " location
+read -p "Continent: " continent
+read -p "Accession Number: " accession
+read -p "Notes: " notes
+read -p "Forward primer: " forward
+read -p "Reverse primer: " reverse
+read -p "Primer (V4 or V3-V4): " primer
+read -p "Age (Adults/Children/Both): " age
+read -p "Sex (Male/Female/Both): " sex
+read -p "Disease: " disease
+read -p "Disease Group: " disease_group
+read -p "Initials: " initials
+
+# generate metadata file name
+metadata_file="$name""-""metadata.txt"
+
+# generate manifest file name
+manifest_file="$name""-""manifest.txt"
+counter=1
+
+# headers for manifest file
+echo -e "sample-id\tabsolute-filepath" > "$manifest_file"
+
+# generate temp directory for fastq files
+mkdir "$accession""-fastq"
+cd "$accession""-fastq"
+
+# grab 30 sample accessions from project accession
+esearch -db sra -query $accession | efetch -format runinfo | cut -d "," -f 1 > $accession".csv"
+tail -n +2 $accession".csv" > temp.csv && mv temp.csv $accession".csv"
+head -n 30 $accession".csv" > temp.csv && mv temp.csv $accession".csv"
+
+# download sample accessions
+while IFS= read -r sample
+do
+  mkdir "$sample""-fastq"
+  cd "$sample""-fastq"
+  prefetch -q $sample
+  fasterq-dump -q $sample > /dev/null
+  mv "$sample""_1.fastq" ..
+  rm "$sample""_2.fastq"
+  cd ..
+  rm -rf "$sample""-fastq"
+done < $accession".csv"
+
+# cleanup
+rm $accession".csv"
+mv * ..
+cd ..
+rmdir "$accession""-fastq"
+
+# write sample-ids/filepaths to manifest file
+# write sample-ids and other info to metadata file
+for file in *fastq; do
+    filepath="$PWD""/""$file"
+    filename="$name""-""$counter"
+    sample_id="${filename%.*}"
+    echo -e "$sample_id\t$filepath" >> "$manifest_file"
+    echo -e "$sample_id\t$notes\t$primer\t$forward\t$reverse\t$location\t$initials\t$age\t$sex\t$disease\t$accession" >> "$metadata_file"
+
+    ((counter+=1))
+done
+
+echo "Manifest file '$manifest_file' created."
+echo "Metadata file '$metadata_file' created."
+
+# activate qiime
+source ~/.zshrc
+conda activate qiime2-2023.7
+
+# generate demux
+qiime tools import \
+    --type 'SampleData[SequencesWithQuality]' \
+    --input-path "$name"-manifest.txt \
+    --output-path "$name"-demux.qza \
+    --input-format SingleEndFastqManifestPhred33V2
+
+# generate demux visualization
+qiime demux summarize \
+    --i-data "$name"-demux.qza \
+    --o-visualization "$name"-demux.qzv
+
+# generate quality scores for deblur
+qiime quality-filter q-score \
+    --i-demux "$name"-demux.qza \
+    --o-filtered-sequences "$name"-demux-filtered.qza \
+    --o-filter-stats "$name"-demux-filter-stats.qza
+
+# run deblur at 150bp
+qiime deblur denoise-16S \
+  --i-demultiplexed-seqs "$name"-demux-filtered.qza \
+  --p-trim-length 150 \
+  --p-left-trim-len 0 \
+  --p-jobs-to-start 4 \
+  --o-representative-sequences "$name"-rep-seqs-deblur.qza \
+  --o-table "$name"-table-deblur.qza \
+  --p-sample-stats \
+  --o-stats "$name"-deblur-stats.qza
+
+# move fastq files to fastq-files subdirectory
+mkdir "$name"-fastq-files
+mv *fastq "$name"-fastq-files
+
+# move all files to single directory
+mkdir "$name"
+mv fastq-files "$name"
+mv deblur.log "$name"
+mv "$name"* "$name"
